7ab05a56387c0956b1bc7445fd121c09295bfbff
archie-mckenzie <archie@generaltranslation.com>
2024-07-18 13:11:44 -0700

    docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,6 +3,11 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 import Image from 'next/image'
 
 const config: DocsThemeConfig = {
+  useNextSeoProps() {
+    return {
+      titleTemplate: `Docs — %s`
+    }
+  },
   logo: <Image src={'/light-logo.png'} width={45} height={45} alt='GT, Inc.'/>,
   project: {
     link: 'https://github.com/General-Translation/gt-react',
