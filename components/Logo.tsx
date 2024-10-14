0f4e1ccc850505e6b33fd2c11513f57f86d230c6
archie-mckenzie <archie@generaltranslation.com>
2024-10-13 18:54:43 -0700

    logo flickering

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,21 +3,21 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { useTheme } from 'next-themes';
-import { useEffect, useState } from 'react';
+import { useLayoutEffect, useState } from 'react';
 
 export default function Logo() {
     const { resolvedTheme } = useTheme();
     const [logoSrc, setLogoSrc] = useState('');
 
-    useEffect(() => {
+    useLayoutEffect(() => {
         setLogoSrc(resolvedTheme === "dark" ? '/gt-logo-dark.svg' : '/gt-logo-light.svg');
     }, [resolvedTheme]);
-    
+
     if (!logoSrc) return;
 
     return (
         <Link className="flex items-center gap-2 text-lg font-semibold" href="/">
-            <Image src={logoSrc} width={40} height={40} alt='GT, Inc.'/>
+            <Image src={logoSrc} width={35} height={35} alt='GT, Inc.'/>
             <span className="sr-only">General Translation, Inc.</span>
         </Link>
     )
