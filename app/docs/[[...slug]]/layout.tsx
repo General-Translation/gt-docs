35a5617f8ab3a9485b8d00b5774f8b35cc67c8d0
Brian Lou <brianlou2002@gmail.com>
2025-01-24 15:01:35 -0800

    add apikey section

--- app/docs/[[...slug]]/layout.tsx
+++ app/docs/[[...slug]]/layout.tsx
@@ -8,23 +8,25 @@ export default function Layout({ children }: { children: ReactNode }) {
     <DocsLayout
       sidebar={{
         banner: (
-          <a
-            href="https://github.com/General-Translation/gt-next"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="px-4 py-2 bg-gradient-to-r from-blue-500/10 to-purple-500/10 rounded-lg border border-blue-100/20">
-              <h3 className="font-semibold text-sm flex items-center gap-2">
-                <img
-                  src="/github-mark.svg"
-                  width={20}
-                  height={20}
-                  alt="GitHub"
-                />
-                Star us on GitHub!
-              </h3>
-            </div>
-          </a>
+          <>
+            <a
+              href="https://github.com/General-Translation/gt-next"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="px-4 py-2 mb-2 bg-gradient-to-r from-blue-500/10 to-purple-500/10 rounded-lg border border-blue-100/20">
+                <h3 className="font-semibold text-sm flex items-center gap-2">
+                  <img
+                    src="/github-mark.svg"
+                    width={20}
+                    height={20}
+                    alt="GitHub"
+                  />
+                  Star us on GitHub!
+                </h3>
+              </div>
+            </a>
+          </>
         ),
       }}
       tree={source.pageTree}
