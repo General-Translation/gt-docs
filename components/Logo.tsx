f530b94cb5bd83103d0decd9207967642c60e85b
Brian Lou <brianlou2002@gmail.com>
2025-03-12 20:25:33 -0700

    bruh

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,24 +1,25 @@
-'use client'
+'use client';
 
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from 'next/image';
 import { useTheme } from 'next-themes';
 import { useLayoutEffect, useState } from 'react';
 
 export default function Logo() {
-    const { resolvedTheme } = useTheme();
-    const [logoSrc, setLogoSrc] = useState('');
+  const { resolvedTheme } = useTheme();
+  const [logoSrc, setLogoSrc] = useState('');
 
-    useLayoutEffect(() => {
-        setLogoSrc(resolvedTheme === "dark" ? '/gt-logo-dark.svg' : '/gt-logo-light.svg');
-    }, [resolvedTheme]);
+  useLayoutEffect(() => {
+    setLogoSrc(
+      resolvedTheme === 'dark' ? '/gt-logo-dark.svg' : '/gt-logo-light.svg'
+    );
+  }, [resolvedTheme]);
 
-    if (!logoSrc) return;
+  if (!logoSrc) return;
 
-    return (
-        <span className="flex items-center gap-2 text-lg font-semibold">
-            <Image src={logoSrc} width={35} height={35} alt='GT, Inc.'/>
-            <span className="sr-only">General Translation, Inc.</span>
-        </span>
-    )
+  return (
+    <span className="flex items-center gap-2 text-lg font-semibold">
+      <Image src={logoSrc} width={35} height={35} alt="GT, Inc." />
+      <span className="sr-only">General Translation, Inc.</span>
+    </span>
+  );
 }
